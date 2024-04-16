37f8ee1aaa0db2f8e1e2aaeda5d9787810a27436
Adolfo Bustillos <juan-adolfo.bustillos@eviden.com>
2024-04-16 01:20:08 -0600

    first

---
+++ src/test/java/com/juadolfob/serviceSDK/ServiceSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juadolfob.serviceSDK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
